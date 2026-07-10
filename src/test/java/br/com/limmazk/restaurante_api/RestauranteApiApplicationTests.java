d8dc973f6d416b5b4341f646f932b333ada13137
Arthur Lima <zklimma07@gmail.com>
2026-07-10 14:47:19 -0300

    firts commit

---
+++ src/test/java/br/com/limmazk/restaurante_api/RestauranteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.limmazk.restaurante_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
