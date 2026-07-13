14e5ba478446a83dab6618911a6f1c518a1405a6
Arthur Lima <zklimma07@gmail.com>
2026-07-13 11:34:30 -0300

    Criando o controller

--- src/main/java/br/com/limmazk/restaurante_api/controller/DishController.java
+++ src/main/java/br/com/limmazk/restaurante_api/controller/DishController.java
@@ -1,9 +1,50 @@
 package br.com.limmazk.restaurante_api.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import br.com.limmazk.restaurante_api.dto.DishRequestDTO;
+import br.com.limmazk.restaurante_api.dto.DishResponseDTO;
+import br.com.limmazk.restaurante_api.model.Dish;
+import br.com.limmazk.restaurante_api.service.DishService;
+import jakarta.validation.Valid;
+import org.hibernate.validator.cfg.defs.UUIDDef;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/dish")
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/dishes")
 public class DishController {
 
+    private final DishService dishService;
 
+    public DishController(DishService dishService){
+        this.dishService = dishService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DishResponseDTO>> getAll(){
+        return ResponseEntity.ok(dishService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DishResponseDTO> getById(@PathVariable UUID id){
+        return ResponseEntity.ok(dishService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<DishResponseDTO> saveDish(@RequestBody @Valid DishRequestDTO dto){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(dishService.create(dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id){
+
+        dishService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
