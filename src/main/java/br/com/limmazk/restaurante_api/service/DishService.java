6ab91b95d419dfd6cc9a054260cde42319d4f524
Arthur Lima <zklimma07@gmail.com>
2026-07-13 10:34:57 -0300

    Finalizando o Service, criando o update e o delete junto com uma nova Exception

--- src/main/java/br/com/limmazk/restaurante_api/service/DishService.java
+++ src/main/java/br/com/limmazk/restaurante_api/service/DishService.java
@@ -2,6 +2,7 @@ package br.com.limmazk.restaurante_api.service;
 
 import br.com.limmazk.restaurante_api.dto.DishRequestDTO;
 import br.com.limmazk.restaurante_api.dto.DishResponseDTO;
+import br.com.limmazk.restaurante_api.exception.ResourceNotFoundException;
 import br.com.limmazk.restaurante_api.mapper.DishMapper;
 import br.com.limmazk.restaurante_api.model.Dish;
 import br.com.limmazk.restaurante_api.repository.DishRepository;
@@ -29,24 +30,35 @@ public class DishService {
 
     public DishResponseDTO findById(UUID id){
         Dish dish = dishRepository.findById(id)
-            .orElseThrow(() -> new RuntimeException("Not found ID."));
-        return DishMapper.toResponseDTO(dish); /*
-        anotacao para estudo, aqui como o mapper ta em static, nao precisa injetar aqui,
-        e no return era obrigado retornar um ResponseDTO, pq disse no começo do metódo que
-        seria um ResponseDTO, então chamei o Mapper e usei uma function dele, que é o toResponse,
-        e ai transformei o dish(que é a Entity) em DTO utilizando esse metodo pronto.
-        */
+            .orElseThrow(() -> new ResourceNotFoundException("Dish not found."));
+        return DishMapper.toResponseDTO(dish);
     }
 
     public DishResponseDTO create(DishRequestDTO dto){
         Dish dish = DishMapper.toEntity(dto);
         Dish savedDish = dishRepository.save(dish);
-        return DishMapper.toResponseDTO(savedDish); /*
-        anotar que esse código recebe o ResquestDTO, que o cliente manda pra aplicação, e
-        o metodo toEntity do mapper, faz a mudança de DTO para Entity, e assim consegue salvar no banco de dados.
-        Mas depois que a Entity é salva no banco de dados, automaticamente já tem um return mandando de volta essa entity
-        que foi transformada em ResponseDTO por meio do metodo do Mapper, que faz essa funçao.
-        NAO ESQUECER DE CRIAR A PASTA DE EXCEPTIONS E VER O EXCEPTION PARA ADICIONAR AQUI
-        */
+        return DishMapper.toResponseDTO(savedDish);
+    }
+
+    public DishResponseDTO update(UUID id, DishRequestDTO dto){
+        Dish dish = dishRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("ID not found."));
+
+        dish.setName(dto.name());
+        dish.setDescription(dto.description());
+        dish.setPrice(dto.price());
+        dish.setPreparationTime(dto.preparationTime());
+        dish.setCategory(dto.category());
+        dish.setAvailable(dto.available());
+
+        dishRepository.save(dish);
+
+        return DishMapper.toResponseDTO(dish);
+    }
+
+    public void delete(UUID id){
+        Dish dish = dishRepository.findById(id)
+                        .orElseThrow(() -> new ResourceNotFoundException("ID not found."));
+        dishRepository.delete(dish);
     }
 }
